8a22df6181635fd904a5440261e045a5935e6911
Chayapol Huang <69767104+jianchayapol@users.noreply.github.com>
2022-01-06 17:24:42 +0700

    Add files via upload

---
+++  8x8 Farming Game/sampleQuiz3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace sampleQuiz3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.start = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.upload = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(194, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(22, 71);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(144, 45);
+            this.start.TabIndex = 1;
+            this.start.Text = "start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "0.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(20, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ยาฆ่าแมลง";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 69);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "เปลี่ยนเป็นยาฆ่าหญ้า";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(22, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(27, 619);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(144, 45);
+            this.save.TabIndex = 6;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // upload
+            // 
+            this.upload.Location = new System.Drawing.Point(22, 550);
+            this.upload.Name = "upload";
+            this.upload.Size = new System.Drawing.Size(144, 45);
+            this.upload.TabIndex = 7;
+            this.upload.Text = "upload";
+            this.upload.UseVisualStyleBackColor = true;
+            this.upload.Click += new System.EventHandler(this.upload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 832);
+            this.Controls.Add(this.upload);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button upload;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++  8x8 Farming Game/sampleQuiz3/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sampleQuiz3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Height = 400;
+            pictureBox1.Width = 400;
+            this.Width = 600;
+            this.Height = 500;
+            label2.ForeColor = Color.BlueViolet;
+        }
+
+        int time = 0;
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            collection.Clear();
+            time = 0;
+            timer1.Start();
+            Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for(int i=0; i < 400 ; i+= 50)
+            {
+                g.DrawLine(Pens.Gray, new Point(0, i), new Point(400, i));
+            }
+            for (int i = 0; i < 400; i += 50)
+            {
+                g.DrawLine(Pens.Gray, new Point(i, 0), new Point(i,400));
+            }
+
+            foreach(Point p in collection.Keys)
+            {
+                string t = collection[p];
+                if (t.Equals("grass"))
+                {
+                    g.DrawString(t, new Font("Arial", 10), Brushes.Green, p.X*50 +7, p.Y*50 +15);
+                }
+                else if (t.Equals("bug"))
+                {
+                    g.DrawString(t, new Font("Arial", 10), Brushes.Gold, p.X*50 +10, p.Y*50 +15);
+                }
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            label1.Text = ((double)time / 10.0).ToString();
+            if (time % 5 == 0)
+            {
+                fillWord();
+
+                if (collection.Count == 64)
+                {
+                    MessageBox.Show("ENDED");
+                    timer1.Stop();
+                }
+            }
+                Refresh();
+        }
+
+        Random random = new Random();
+
+        Dictionary<Point, string> collection = new Dictionary<Point, string>();
+        
+
+
+        public void fillWord()
+        {
+            int i = random.Next(0, 8);
+            int j = random.Next(0, 8);
+
+            while (collection.ContainsKey(new Point(i,j)) && collection.Count <= 64)
+            {
+                i = random.Next(0, 8);
+                j = random.Next(0, 8);
+            }
+
+            Point tmp = new Point(i, j);
+
+            int idx = random.Next(0, 2);
+            if (idx % 2 == 0)
+            {
+                collection.Add(tmp, "grass");
+            }
+            else
+            {
+                collection.Add(tmp, "bug");
+            }
+
+        }
+
+        bool isYaKaYa = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isYaKaYa = !isYaKaYa;
+            if (isYaKaYa)
+            {
+                label2.Text = "ยาฆ่าหญ้า";
+                label2.ForeColor = Color.Red;
+                button1.Text = "เปลี่ยนเป็นยาฆ่าแมลง";
+            }
+            else
+            {
+                label2.Text = "ยาฆ่าแมลง";
+                label2.ForeColor = Color.BlueViolet;
+                button1.Text = "เปลี่ยนเป็นยาฆ่าหญ้า";
+            }
+
+            }
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            string ya;
+            if (isYaKaYa) ya = "grass";
+            else ya = "bug";
+            int x = (e.X / 50);
+            int y = (e.Y / 50);
+
+            Point tmp = new Point(x, y);
+
+            //MessageBox.Show(x.ToString() + " " + y.ToString());
+
+            if (collection.ContainsKey(tmp) && collection[tmp]==ya)
+            {
+                collection.Remove(tmp);
+            }
+
+            //MessageBox.Show(e.X.ToString() + " " + e.Y.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+        private void upload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = "";
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Title = "Open CSV File";
+                dialog.Filter = "CSV Files (*.csv)|*.csv";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    filename = dialog.FileName;
+                }
+                else return;
+
+
+                // Read a text file line by line.  
+                string[] lines = System.IO.File.ReadAllLines(filename);
+
+                collection.Clear();
+
+                foreach (string line in lines)
+                {
+                    int x = Int16.Parse(line.Split(',')[0]);
+                    int y = Int16.Parse(line.Split(',')[1]);
+                    string text = line.Split(',')[2];
+                    if (line != "\r\n") collection.Add(new Point(x, y),text);
+                }
+                pictureBox1.Refresh();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            System.IO.StreamWriter writer = null;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                writer = new System.IO.StreamWriter(saveFileDialog1.FileName);
+
+                foreach (Point p in collection.Keys)
+                {
+                    writer.Write(p.X.ToString() + "," + p.Y.ToString() + "," + collection[p] + "\r\n");
+                }
+                writer.Close();
+            }
+
+        }
+    }
+}
