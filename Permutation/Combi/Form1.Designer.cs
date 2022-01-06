37c3d0819eeef12f13b5f9ba15a5409cc2397a2d
Chayapol Huang <69767104+jianchayapol@users.noreply.github.com>
2022-01-06 17:17:39 +0700

    Add files via upload

---
+++ Permutation/Combi/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Combi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "🎃 Combi 😈";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(73, 220);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Gen Test1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Bahnschrift SemiBold", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(73, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Gen Test2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift SemiLight", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(280, 207);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(496, 551);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(796, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "1. วิธีจัดที่นั่งหน้ากระดาน ให้กับ ผช 5 คน, ผญ 5 คน โดย ผญ ต้องมี ผญ นั่งข้างๆ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(37, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(739, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "2. เรียงลูกแก้ว 7 ลูก ( แดง 2, เขียว 2, ขาว 3) โดย 2 ลูกที่ติดกันต้องสีต่างกัน";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 785);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Permutation/Combi/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<List<string>> test1Ans = new List<List<string>>();
+        List<List<string>> test2Ans = new List<List<string>>();
+
+        private int findAndCount(List<string> s, string c)
+        {
+            int count = 0;
+            foreach (string e in s)
+            {
+                if (e == c) count++;
+            }
+            return count;
+        }
+
+        private List<string> copy(List<string> from, List<string> to)
+        {
+            foreach(string e in from)
+            {
+                to.Add(e);
+            }
+            return to;
+        }
+        private void sitting(List<string> s)
+        {
+            if (s.Count() == 10)
+            {
+                test1Ans.Add(s);
+                return;
+            }
+
+            if (s.Count() > 1 && s.ElementAt(s.Count() - 1) == "G" && s.ElementAt(s.Count() - 2) != "G")
+            {
+                if (findAndCount(s, "G") < 5)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("G");
+                    sitting(tmp);
+                }
+            }
+            else if (s.Count() == 1 && s.ElementAt(s.Count() - 1) == "G")
+            {
+                if (findAndCount(s, "G") < 5)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("G");
+                    sitting(tmp);
+                }
+            }
+
+            else if (s.Count() == 9 && s.ElementAt(s.Count() - 1) != "G")
+            {
+                if (findAndCount(s, "B") < 5)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("B");
+                    sitting(tmp);
+                }
+            }
+            else
+            {
+                if (findAndCount(s, "G") < 5)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s,tmp);
+                    tmp.Add("G");
+                    sitting(tmp);
+                }
+                if (findAndCount(s, "B") < 5)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("B");
+                    sitting(tmp);
+                }
+            }
+        }
+
+        private void lookkaew(List<string> s)
+        {
+            if (s.Count() == 7)
+            {
+                test2Ans.Add(s);
+                return;
+            }
+
+            // R R , G G ,  W W W 
+
+            if (s.Count() > 0 && s.ElementAt(s.Count() - 1) == "R")
+            {
+                if (findAndCount(s, "G") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("G");
+                    lookkaew(tmp);
+                }
+                if (findAndCount(s, "W") < 3)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("W");
+                    lookkaew(tmp);
+                }
+            }
+            else if(s.Count() > 0 && s.ElementAt(s.Count() - 1) == "G")
+            {
+                if (findAndCount(s, "R") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("R");
+                    lookkaew(tmp);
+                }
+                if (findAndCount(s, "W") < 3)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("W");
+                    lookkaew(tmp);
+                }
+            }
+
+            else if (s.Count() > 0 && s.ElementAt(s.Count() - 1) == "W")
+            {
+                if (findAndCount(s, "R") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("R");
+                    lookkaew(tmp);
+                }
+                if (findAndCount(s, "G") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("G");
+                    lookkaew(tmp);
+                }
+            }
+
+            else
+            {
+                if (findAndCount(s, "R") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("R");
+                    lookkaew(tmp);
+                }
+                if (findAndCount(s, "G") < 2)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("G");
+                    lookkaew(tmp);
+                }
+                if (findAndCount(s, "W") < 3)
+                {
+                    List<string> tmp = new List<string>();
+                    tmp = copy(s, tmp);
+                    tmp.Add("W");
+                    lookkaew(tmp);
+                }
+            }
+
+
+        }
+
+        String show_list(List<String> l)
+        {
+            String output = "{ ";
+            for (int i = 0; i < l.Count; i++)
+            {
+                output += l[i].ToString();
+                if(i!=l.Count-1) output += ", ";
+
+            }
+            output += " }";
+
+            return output;
+
+
+        }
+        String show_result(List<List<String>> l)
+        {
+            String output = "จำนวนวิธี  = " + l.Count.ToString() + " วิธี \r\n";
+            output += " {\r\n";
+            for (int i = 0; i < l.Count; i++)
+            {
+                output += "    " + show_list(l[i]) + " ,\r\n";
+
+            }
+            output += " }\r\n";
+
+            return output;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            test1Ans.Clear();
+            List<string> es = new List<string>();
+            sitting(es);
+            textBox1.Text = show_result(test1Ans);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            test2Ans.Clear();
+            List<string> es = new List<string>();
+            lookkaew(es);
+            textBox1.Text = show_result(test2Ans);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
